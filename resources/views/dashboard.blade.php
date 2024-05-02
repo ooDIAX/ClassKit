82118ce0b277f49facd507f41c18913fb3b76906
Niko <sharikadze.nikoloz03@gmail.com>
2024-05-03 01:01:09 +0700

    make whole button clickable

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,9 +11,9 @@
                         <div class="p-6 text-gray-800 dark:text-gray-100">
                             <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
                                 @foreach ($enrolledCourses as $course)
-                                    <div class="border border-gray-300 rounded-md p-4 hover:border-gray-500 transition duration-300">
-                                        <a href="{{ route('courses.show', $course) }}">{{ $course->name }}</a>
-                                    </div>
+                                    <a href="{{ route('courses.show', $course) }}" class="border border-gray-300 rounded-md p-4 hover:border-gray-500 transition duration-300 block">
+                                        {{ $course->name }}
+                                    </a>
                                 @endforeach
                             </div>
                         </div>
